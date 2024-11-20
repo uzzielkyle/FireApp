f738b921b8ca94f5316f438df62ff1d577dce76a
UzziKyle <uzzielkyleynciong0824@gmail.com>
2024-11-20 10:10:18 +0800

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from fire.views import HomePageView, ChartView
+from fire import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+    path("dashboard_chart", ChartView.as_view(), name="dashboard-chart")
+]
---
+++ fire/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from fire.models import Locations, Incident
+
+
+class HomePageView(ListView):
+    model = Locations
+    context_object_name = 'home'
+    template_name = "home.html"
+
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
